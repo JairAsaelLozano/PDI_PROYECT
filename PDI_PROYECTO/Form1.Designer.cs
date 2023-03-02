7cfafb5432dad1ffed484691a504d8fba1967415
JairAsaelLozano <74801997+JairAsaelLozano@users.noreply.github.com>
2023-03-02 12:30:11 -0600

    pantalla

---
+++ PDI_PROYECTO/Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace PDI_PROYECTO
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            comboBox1 = new ComboBox();
+            comboBox2 = new ComboBox();
+            label2 = new Label();
+            pictureBox1 = new PictureBox();
+            pictureBox2 = new PictureBox();
+            button1 = new Button();
+            button2 = new Button();
+            radioButton1 = new RadioButton();
+            radioButton2 = new RadioButton();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe Script", 15.75F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point);
+            label1.Location = new Point(78, 67);
+            label1.Name = "label1";
+            label1.Size = new Size(115, 34);
+            label1.TabIndex = 0;
+            label1.Text = "Camara: ";
+            // 
+            // comboBox1
+            // 
+            comboBox1.FlatStyle = FlatStyle.System;
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(78, 104);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(121, 28);
+            comboBox1.TabIndex = 1;
+            // 
+            // comboBox2
+            // 
+            comboBox2.FlatStyle = FlatStyle.System;
+            comboBox2.FormattingEnabled = true;
+            comboBox2.Location = new Point(78, 209);
+            comboBox2.Name = "comboBox2";
+            comboBox2.Size = new Size(121, 28);
+            comboBox2.TabIndex = 3;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe Script", 15.75F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point);
+            label2.Location = new Point(78, 172);
+            label2.Name = "label2";
+            label2.Size = new Size(91, 34);
+            label2.TabIndex = 2;
+            label2.Text = "Filtro: ";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new Point(318, 73);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(169, 164);
+            pictureBox1.TabIndex = 4;
+            pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Location = new Point(566, 73);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(169, 164);
+            pictureBox2.TabIndex = 5;
+            pictureBox2.TabStop = false;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.OldLace;
+            button1.FlatStyle = FlatStyle.Popup;
+            button1.Font = new Font("Segoe Script", 15.75F, FontStyle.Bold, GraphicsUnit.Point);
+            button1.ForeColor = SystemColors.ControlText;
+            button1.Location = new Point(339, 243);
+            button1.Name = "button1";
+            button1.Size = new Size(135, 40);
+            button1.TabIndex = 6;
+            button1.Text = "Capturar";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.OldLace;
+            button2.FlatStyle = FlatStyle.Popup;
+            button2.Font = new Font("Segoe Script", 15.75F, FontStyle.Bold, GraphicsUnit.Point);
+            button2.ForeColor = SystemColors.ControlText;
+            button2.Location = new Point(588, 243);
+            button2.Name = "button2";
+            button2.Size = new Size(135, 40);
+            button2.TabIndex = 7;
+            button2.Text = "Guardar";
+            button2.UseVisualStyleBackColor = false;
+            // 
+            // radioButton1
+            // 
+            radioButton1.AutoSize = true;
+            radioButton1.Location = new Point(78, 282);
+            radioButton1.Name = "radioButton1";
+            radioButton1.Size = new Size(84, 24);
+            radioButton1.TabIndex = 8;
+            radioButton1.TabStop = true;
+            radioButton1.Text = "Fotografá";
+            radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            radioButton2.AutoSize = true;
+            radioButton2.Location = new Point(78, 312);
+            radioButton2.Name = "radioButton2";
+            radioButton2.Size = new Size(63, 24);
+            radioButton2.TabIndex = 9;
+            radioButton2.TabStop = true;
+            radioButton2.Text = "Video";
+            radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Wheat;
+            BackgroundImageLayout = ImageLayout.None;
+            ClientSize = new Size(758, 369);
+            Controls.Add(radioButton2);
+            Controls.Add(radioButton1);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(pictureBox2);
+            Controls.Add(pictureBox1);
+            Controls.Add(comboBox2);
+            Controls.Add(label2);
+            Controls.Add(comboBox1);
+            Controls.Add(label1);
+            Font = new Font("Segoe Script", 9F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point);
+            FormBorderStyle = FormBorderStyle.SizableToolWindow;
+            Margin = new Padding(3, 4, 3, 4);
+            Name = "Form1";
+            Text = "FilterApp";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private ComboBox comboBox1;
+        private ComboBox comboBox2;
+        private Label label2;
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox2;
+        private Button button1;
+        private Button button2;
+        private RadioButton radioButton1;
+        private RadioButton radioButton2;
+    }
+}
---
+++ PDI_PROYECTO/Form1.cs
@@ -0,0 +1,25 @@
+namespace PDI_PROYECTO
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void tabControl1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tabPage2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
